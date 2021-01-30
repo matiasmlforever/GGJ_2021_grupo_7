ca4d7a10ad1af8eb0b08d10fe5a085455e602989
Matias Lecaros <mlecaros@digital.gob.cl>
2021-01-30 13:15:14 -0300

    DialogBox with typing characters effect

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,14 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
     public Queue<string> sentences;
+    public bool drawSentence = false;
+    public string drawingSentence;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            // execute block of code here
+            if (drawSentence)
+            {
+                dialogueText.text = dialogueText.text.Insert(dialogueText.text.Length, drawingSentence[0].ToString());
+                drawingSentence = drawingSentence.Substring(1);
+                if (drawingSentence.Length == 0) {
+                    drawSentence = false;
+                }
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sentences = new Queue<string>();
+    }
+
+    internal void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = "";
+        drawSentence = true;
+        drawingSentence = sentence;
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
     }
 }
