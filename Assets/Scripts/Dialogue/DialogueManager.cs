4153573420ad6fb61a2ddb7a99916f7f7fdc576a
Matias Lecaros <mlecaros@digital.gob.cl>
2021-01-30 11:10:55 -0300

    Moved camera out of screen prefab

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
